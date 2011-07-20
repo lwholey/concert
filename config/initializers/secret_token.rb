4f00aea75acd385237e50b5f86f34a44d466cfa3
Leonard Wholey <lwholey@gmail.com>
2011-07-20 19:13:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concert::Application.config.secret_token = '6c0fb6e2db191ec08de967567c8ac013a32c410968ea6ea3b128af60ac75ad4762635d0473f32960ed7b7394481e5e0347c13b6852b6381438d5aba67695ab85'
