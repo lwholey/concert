79b0d4a9bcf8bed020bc618a6ac701f3c959c345
Leonard Wholey <lwholey@gmail.com>
2011-08-02 20:04:53 -0400

    Follow ROR tutorial chp 1 advice to fix rake problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110716132555) do
+ActiveRecord::Schema.define(:version => 20110717013358) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
