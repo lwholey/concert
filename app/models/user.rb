a3209ef05e7f89ceea93d90f9983eca777e8524b
Paul Sutcliffe <frpaulas@gmail.com>
2011-08-06 10:55:28 -0400

    changed to mongodb
    Signed-off-by: Paul Sutcliffe <frpaulas@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# require 'digest'
 # == Schema Information
 #
 # Table name: users
@@ -8,11 +9,31 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
+# 
+# class User < ActiveRecord::Base
+#   attr_accessible :name
+#   
+#   validates :name,  :presence => true
+# #                    :length   => { :minimum => 10 }
+# end
+load "lib/mongoid_profileable.rb"
 
-require 'digest'
-class User < ActiveRecord::Base
-  attr_accessible :name
+class User
+  include Mongoid::Document
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   
-  validates :name,  :presence => true
-#                    :length   => { :minimum => 10 }
+  field :name
+    validates_presence_of :name
+  field :email
+    validates_uniqueness_of :email, :message => "That login has already beed taken", :case_sensitive => false, :on => :create
+    validates_presence_of   :email, :message => "You must have a login ID", :on => :create
+  field :password
+    validates_presence_of   :password, :message => "You must supply a password", :on => :create
+
+  devise  :database_authenticatable, :registerable, :timeoutable, 
+          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 end
