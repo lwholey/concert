56418bc85e783d9ab1fe5d94385e30890b868874
Leonard Wholey <lwholey@gmail.com>
2011-08-14 14:45:13 -0400

    Remove unused variables in table salt and encrypted password

---
+++ db/migrate/20110814184303_remove_encrypted_password_from_users.rb
@@ -0,0 +1,9 @@
+class RemoveEncryptedPasswordFromUsers < ActiveRecord::Migration
+  def self.up
+    remove_column :users, :encrypted_password
+  end
+
+  def self.down
+    add_column :users, :encrypted_password, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110814135704) do
+ActiveRecord::Schema.define(:version => 20110814184303) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password"
-    t.string   "salt"
     t.string   "maxNumberOfBands"
   end
 
