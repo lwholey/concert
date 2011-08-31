714c483ecc5d02f24c63586c52982b7d4462be51
John Wilde <johnwilde@gmail.com>
2011-08-31 16:02:59 -0700

    Modify footer.
    Changed spotify logot (using a smaller one) and rearranged
    layout to show the Spotify and Eventful icons and blurbs side-by-side.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(:version => 20110831125328) do
 
+  create_table "searches", :force => true do |t|
+    t.string   "form_location"
+    t.string   "form_musiccategory"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
