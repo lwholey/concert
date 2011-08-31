55cb77a437453f9670378e656acb08e8a90bb0a6
Leonard Wholey <lwholey@gmail.com>
2011-08-31 10:06:08 -0400

    Change web page title to lennylonglegs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   
   #Return a title on a per-page basis
   def title
-    base_title = "Eventful and Spotify Mashup"
+    base_title = "lennylonglegs"
     if @title.nil?
       base_title
     else
